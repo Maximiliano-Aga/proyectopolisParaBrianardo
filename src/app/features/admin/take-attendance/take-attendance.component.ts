2063e92375ee1070e6b3cc5f1f60ddda18fea771
agamaximiliano <agamaximiliano@gmail.com>
2025-07-27 19:28:37 -0300

    login terminado

--- src/app/features/admin/take-attendance/take-attendance.component.ts
+++ src/app/features/admin/take-attendance/take-attendance.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-take-attendance',
   imports: [],
--- src/app/features/auth/password-recovery/password-recovery.component.ts
+++ src/app/features/auth/password-recovery/password-recovery.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import {RouterLink} from '@angular/router';
 @Component({
   selector: 'app-password-recovery',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './password-recovery.component.html',
   styleUrl: './password-recovery.component.css'
 })
