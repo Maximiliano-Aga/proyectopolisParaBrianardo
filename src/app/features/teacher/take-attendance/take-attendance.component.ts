2ca0ce7a604f951316635e360d83fe25ddc14199
agamaximiliano <agamaximiliano@gmail.com>
2025-09-28 19:55:03 -0300

    cambio en asistencias estudiante

--- src/app/features/teacher/take-attendance/take-attendance.component.ts
+++ src/app/features/teacher/take-attendance/take-attendance.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-take-attendance',
-  imports: [],
-  templateUrl: './take-attendance.component.html',
-  styleUrl: './take-attendance.component.css'
+  selector: 'app-take-attendance',
+  templateUrl: './take-attendance.component.html',
+  styleUrl: './take-attendance.component.css',
+  standalone: true, // asumiendo que tu componente es standalone
+  imports: [FormsModule, CommonModule]
 })
-export class TakeAttendanceComponent {
+export class TakeAttendanceComponent implements OnInit {
 
+  fechaSeleccionada: string;
+
+  constructor() {
+    this.fechaSeleccionada = this.obtenerFechaDeHoy();
+  }
+
+  ngOnInit(): void {
+  }
+
+  obtenerFechaDeHoy(): string {
+    const hoy = new Date();
+    const year = hoy.getFullYear();
+    const month = (hoy.getMonth() + 1).toString().padStart(2, '0');
+    const day = hoy.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
 }
