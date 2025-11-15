780e9269fcbec29bbcdd04ca5e35c6dece1f2312
agamaximiliano <agamaximiliano@gmail.com>
2025-11-15 18:54:18 -0300

    formulario de registro reactivo y manejo de errores

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { studentRoutes } from './features/student/student.routes';
 import { teacherRoutes } from './features/teacher/teacher.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideRouter(studentRoutes),provideRouter(teacherRoutes),provideClientHydration(withEventReplay())]
+  providers: [provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideRouter(studentRoutes),provideRouter(teacherRoutes),provideClientHydration(withEventReplay())]
 };
 
